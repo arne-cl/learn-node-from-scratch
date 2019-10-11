76178ac0d8bfacd1e0a4acbad00af44a17e8d610
Arne Neumann <nodejs.programming@arne.cl>
2019-10-12 00:05:47 +0200

    log all requests to STDOUT

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const routes = require('./routes/index');
 
 // create our Express app
 const app = express();
+
+if (app.get('node_env') == 'production') { // log request errors to file in production
+  app.use(morgan('common', { skip: function(req, res) { return res.statusCode < 400 }, stream: __dirname + '/../morgan.log' }));
+} else { // log all requests to STDOUT in dev mode
+  app.use(morgan('dev'));
+}
 
 // This is how we handle our routes
 app.use('/', routes);
