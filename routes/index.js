0ce2b69451dfc63effe9615718892bb116917bb3
Arne Neumann <nodejs.programming@arne.cl>
2019-10-12 16:44:09 +0200

    added routes which return query params / reverse strings

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,20 @@ router.get('/', (req, res) => {
   res.send('Hey! It works! Echt');
 });
 
+// take a GET request and echo the parameters as JSON.
+// Example: GET /json?name=John&age=23
+//          {"name":"John","age":"23"}
 router.get('/json', (req, res) => {
-  const user = {name: 'John', age: 42};
-  res.json(user);
+  res.json(req.query);
 });
 
+// Return the reversed value of the ?name parameter.
+// Example: GET /reverse/john
+//          nhoj
+router.get('/reverse/:name', (req, res) => {
+  // reverse a string by splitting it into an array, reverse and rejoin into a string
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
 
 module.exports = router;
