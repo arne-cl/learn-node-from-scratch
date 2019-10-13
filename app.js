30792a008567bd5a42d9c9f4dce14165ef74ff58
Arne Neumann <nodejs.programming@arne.cl>
2019-10-13 13:20:18 +0200

    added fancy error handling/visualisation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path'); // access server paths
 
 const helpers = require('./helpers'); // helper functions and constants used by Wes Bos
 const routes = require('./routes/index');
+const errorHandlers = require('./handlers/errorHandlers'); // custom error handlers
 
 // create our Express app
 const app = express();
@@ -36,5 +37,25 @@ app.use((req, res, next) => {
 // This is how we handle our routes.
 app.use('/', routes);
 
-// done! we export it so we can start the site in start.js
+
+// Lots of fancy error handling happens if the routes do not succeed.
+
+// If not found, return a 404.
+app.use(errorHandlers.notFound);
+
+// Check, if it is just a validation error
+app.use(errorHandlers.flashValidationErrors);
+
+// Otherwise this was a really bad error we didn't expect!
+if (app.get('env') === 'development') {
+  /* Development Error Handler - Prints stack trace */
+  app.use(errorHandlers.developmentErrors);
+}
+
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+
+
+// We export the app, so we can use it in start.js.
 module.exports = app;
